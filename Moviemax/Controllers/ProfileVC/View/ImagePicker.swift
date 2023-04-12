4cb0881977c1796568e92ec9fbb74929bd1a3714
DaniilPetrov <dany_petrov@icloud.com>
2023-04-12 14:32:23 +0300

    added change avatar functionality

---
+++ Moviemax/Controllers/ProfileVC/View/ImagePicker.swift
@@ -0,0 +1,95 @@
+//
+//  ImagePicker.swift
+//  Moviemax
+//
+//  Created by Даниил Петров on 12.04.2023.
+//
+
+import UIKit
+
+public protocol ImagePickerDelegate: AnyObject {
+    func didSelect(image: UIImage?)
+}
+
+open class ImagePicker: NSObject {
+
+    private let pickerController: UIImagePickerController
+    private weak var presentationController: UIViewController?
+    private weak var delegate: ImagePickerDelegate?
+
+    public init(presentationController: UIViewController, delegate: ImagePickerDelegate) {
+        self.pickerController = UIImagePickerController()
+
+        super.init()
+
+        self.presentationController = presentationController
+        self.delegate = delegate
+
+        self.pickerController.delegate = self
+        self.pickerController.allowsEditing = true
+        self.pickerController.mediaTypes = ["public.image"]
+    }
+
+    private func action(for type: UIImagePickerController.SourceType, title: String) -> UIAlertAction? {
+        guard UIImagePickerController.isSourceTypeAvailable(type) else {
+            return nil
+        }
+
+        return UIAlertAction(title: title, style: .default) { [unowned self] _ in
+            self.pickerController.sourceType = type
+            self.presentationController?.present(self.pickerController, animated: true)
+        }
+    }
+
+    public func present(from sourceView: UIView) {
+
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+
+        if let action = self.action(for: .camera, title: "Take a photo") {
+            alertController.addAction(action)
+        }
+        if let action = self.action(for: .savedPhotosAlbum, title: "Camera roll") {
+            alertController.addAction(action)
+
+
+        }
+        if let action = self.action(for: .photoLibrary, title: "Choose from your file") {
+            alertController.addAction(action)
+
+        }
+        let deleteAction = UIAlertAction(title: "Delete photo", style: .destructive) { ( action: UIAlertAction) in
+            self.pickerController(self.pickerController, didSelect: UIImage(named: "ProfileImage"))
+        }
+        alertController.addAction(deleteAction)
+        
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
+        self.presentationController?.present(alertController, animated: true)
+    }
+
+    private func pickerController(_ controller: UIImagePickerController, didSelect image: UIImage?) {
+        controller.dismiss(animated: true, completion: nil)
+        self.delegate?.didSelect(image: image)
+    }
+
+}
+
+extension ImagePicker: UIImagePickerControllerDelegate {
+
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.pickerController(picker, didSelect: nil)
+    }
+
+    public func imagePickerController(_ picker: UIImagePickerController,
+                                      didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        guard let image = info[.editedImage] as? UIImage else {
+            return self.pickerController(picker, didSelect: nil)
+        }
+        self.pickerController(picker, didSelect: image)
+    }
+}
+
+extension ImagePicker: UINavigationControllerDelegate {
+
+}
