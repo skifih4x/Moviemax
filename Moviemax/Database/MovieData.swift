87afa4fb3a94f7cddf1a1b26bfb9569e8a2b2a19
AlexZhadaev <pvt.vatnik@gmail.com>
2023-04-12 21:24:13 +0300

    add Realm

---
+++ Moviemax/Database/MovieData.swift
@@ -0,0 +1,28 @@
+//
+//  MovieData.swift
+//  Moviemax
+//
+//  Created by Жадаев Алексей on 11.04.2023.
+//
+
+import Foundation
+import RealmSwift
+
+class MovieData: Object {
+    @Persisted var id: Int
+    @Persisted var title: String
+    @Persisted var releaseDate: String
+    @Persisted var posterPath: String
+    @Persisted var genre: String
+    @Persisted var voteAverage: Double
+    
+    convenience init(id: Int, title: String, releaseDate: String, posterPath: String, genre: String, voteAverage: Double) {
+        self.init()
+        self.id = id
+        self.title = title
+        self.releaseDate = releaseDate
+        self.posterPath = posterPath
+        self.genre = genre
+        self.voteAverage = voteAverage
+    }
+}
